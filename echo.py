27b50c71a2aee0f69597a9f59e341f1edd6f6196
Jordan Davidson <jordandavidson744@yahoo.com>
2020-03-09 16:36:37 -0400

    finished echo.py

--- echo.py
+++ echo.py
@@ -1,22 +1,59 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Jordan Davidson"
 
 
 import sys
+import argparse
+
+
+def to_upper(text):
+    return text.upper()
+
+
+def to_lower(text):
+    return text.lower()
+
+
+def to_title(text):
+    return text.title()
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument('-u', '--upper', 
+                        help='convert text to uppercase', action='store_true')
+    parser.add_argument('-l', '--lower', 
+                        help='convert text to lowercase', action='store_true')
+    parser.add_argument('-t', '--title',
+                        help='convert text to titlecase', action='store_true')
+    return parser
 
 
 def main(args):
-    """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+    text = ns.text
+    upper = ns.upper
+    lower = ns.lower
+    title = ns.title
+    if text is sys.argv[1]:
+        print(text)
+    if upper:
+        print(to_upper(text))
+    elif lower:
+        print(to_lower(text))
+    elif title:
+        print(to_title(text))
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,10 +3,37 @@
 
 import unittest
 import echo
+import subprocess
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
-    pass
+
+    def test_help(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+    def test_upper(self):
+        self.assertIsNotNone(echo.to_upper)
+        self.assertEqual(echo.to_upper('hello!'), 'HELLO!')
+        self.assertEqual(echo.to_upper('WHAT?'), 'WHAT?')
+
+    def test_lower(self):
+        self.assertIsNotNone(echo.to_lower)
+        self.assertEqual(echo.to_lower('HELLO!'), 'hello!')
+        self.assertEqual(echo.to_lower('what?'), 'what?')
+
+    def test_title(self):
+        self.assertIsNotNone(echo.to_title)
+        self.assertEqual(echo.to_title('hello there'), 'Hello There')
+        self.assertEqual(echo.to_title('Hey'), 'Hey')
+
 
 if __name__ == '__main__':
     unittest.main()
